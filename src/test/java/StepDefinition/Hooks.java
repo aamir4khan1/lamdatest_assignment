af6035619311dc531bf96f0165efed992e02b962
Anoop <anoop.1@spinny.com>
2024-04-11 20:15:49 +0530

    headless browser change

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -52,7 +52,7 @@ public class Hooks {
     		mobileEmulation.put("deviceName", cr.valueOnTheKey("DeviceName"));
 
     		ChromeOptions chromeOptions = new ChromeOptions();
-
+    		chromeOptions.addArguments("--headless");
     		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
 
     		driver = new ChromeDriver(chromeOptions);
