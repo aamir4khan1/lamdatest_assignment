65b4280e77242ec00583742ceb3c87508049b7b2
Pankaj Kumar <pankaj.kumar1@spinny.com>
2024-05-24 14:32:58 +0530

    SpinnyAssuredBenefits-PLP

---
+++ src/test/java/StepDefinition/PLPSpinnyBenefitsSection.java
@@ -0,0 +1,40 @@
+
+package StepDefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import Utility.ConfigReader;
+import Utility.Wait;
+import Utility.preMethods;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class PLPSpinnyBenefitsSection {
+
+	public PLPSpinnyBenefitsSection() {
+
+	}
+
+	static ConfigReader cr = new ConfigReader();
+	WebDriver driver = StepDefinition.Hooks.driver;
+	Wait wait = new Wait();
+	static preMethods prm = new preMethods();
+
+	@And("scroll down to spinny benefits section")
+	public void scroll_down_to_spinny_benefits_section() throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll(0,4500)");
+		Thread.sleep(2000);
+	}
+	
+	@And("validate spinny benefits section")
+	public void validate_spinny_benefits_section() throws InterruptedException {
+		WebElement spinnyBenefitsSectionContainer = driver.findElement(By.xpath(cr.valueOnTheKey("plp_spinnyBenefitsSectionBanner")));
+		Assert.assertEquals(true, spinnyBenefitsSectionContainer.isDisplayed());
+		Thread.sleep(2000);
+	}
+}
