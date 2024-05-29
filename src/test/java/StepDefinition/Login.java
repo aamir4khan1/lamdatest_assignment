7e4084fc057400b8542c0c44f7705dfc36bcdde5
Anoop <anoop.1@spinny.com>
2024-05-29 14:31:57 +0530

    login debug

--- src/test/java/StepDefinition/Login.java
+++ src/test/java/StepDefinition/Login.java
@@ -82,6 +82,7 @@ public class Login {
 		//prm.sendTextToTextBox(driver, cr.valueOnTheKey("name"),("xpath"),"Testing");
 		Thread.sleep(1000);
 		prm.click(driver, cr.valueOnTheKey("verify_your_number"), "xpath");
+		Thread.sleep(6000);
 	}
 	@And("user enters otp code")
 	public void user_enters_otp_code() throws InterruptedException {
