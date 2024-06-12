826dda3467eb3137221a1f99c8e435fb9d0048fb
Anoop <anoop.1@spinny.com>
2024-06-12 17:32:14 +0530

    added network request code

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v123.network.Network;
 
 import Utility.ConfigReader;
 
@@ -25,7 +26,7 @@ import java.util.Map;
 
 public class Hooks {
 	static WebDriver driver;
-	//static DevTools devTools;
+	static DevTools devTools;
 	static ConfigReader cr = new ConfigReader();
 
 	@Before
@@ -69,9 +70,9 @@ public class Hooks {
 			chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
 
 			driver = new ChromeDriver(chromeOptions);
-			// devTools = ((HasDevTools) driver).getDevTools();
-	        //devTools.createSession();
-
+			devTools = ((HasDevTools) driver).getDevTools();
+	        devTools.createSession();
+	        devTools.addListener(Network.requestWillBeSent(), request -> { });
 			return driver;
 		}
 	}
