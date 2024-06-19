a2d815a4d995a36ddb1851aa8dca26ff93043385
Pankaj Kumar <pankaj.kumar1@spinny.com>
2024-06-19 12:47:52 +0530

    Fix-BaseStep

--- src/test/java/StepDefinition/BaseSteps.java
+++ src/test/java/StepDefinition/BaseSteps.java
@@ -40,7 +40,13 @@ public class BaseSteps {
 //            System.out.println("Response Status: " + response.getResponse().getStatus());
 //            System.out.println("Response Status Text: " + response.getResponse().getStatusText());
 //        });
-		driver.get(cr.valueOnTheKey(System.getProperty("URL")));
+		
+		if(System.getProperty("URL") != null) {
+			driver.get(cr.valueOnTheKey(System.getProperty("URL")));
+		} else {
+			driver.get(cr.valueOnTheKey("URL"));
+		}
+		
 	}
 
 	@When("^Wait for the application page to load completely$")
