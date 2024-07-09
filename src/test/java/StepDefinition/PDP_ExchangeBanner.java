27b9501a56c55bb304f9045d4dcd3b2e25562bd6
ravina <ravina.arora@spinny.com>
2024-07-09 13:52:12 +0530

    PDP_ExchangeBanner

---
+++ src/test/java/StepDefinition/PDP_ExchangeBanner.java
@@ -0,0 +1,63 @@
+package StepDefinition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+//import cucumberIntegrationTests.screens.iOS.IOSLoginScreen;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.junit.Assert;
+import Utility.ConfigReader;
+import Utility.Wait;
+import Utility.preMethods;
+import cucumber.api.java.en.Then;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+public class PDP_ExchangeBanner {
+	static ConfigReader cr = new ConfigReader();
+    WebDriver driver = StepDefinition.Hooks.driver;
+    Wait wait = new Wait();
+    static preMethods prm = new preMethods();
+    
+    
+    @And ("user select car")
+    public void user_select_car() throws InterruptedException {
+    	Thread.sleep(4000);
+    	prm.click(driver, cr.valueOnTheKey("plp_cardPick"), "xpath");
+		Thread.sleep(2000);
+	
+    }
+
+    @And("user scroll down and validate")
+    public void scroll_down_and_validate() throws InterruptedException {
+    	Thread.sleep(3000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scroll(0,500)");
+		Thread.sleep(2000);
+
+    	WebElement pdp_ExchangeBanner = driver.findElement(By.xpath(cr.valueOnTheKey("pdp_ExchangeBanner")));
+    	Assert.assertEquals(true, pdp_ExchangeBanner.isDisplayed());
+		Thread.sleep(2000);
+		
+		prm.click(driver, cr.valueOnTheKey("pdp_BuyWithExchange"), "xpath");
+		Thread.sleep(2000);
+	
+		WebElement pdp_ExchangePopup = driver.findElement(By.xpath(cr.valueOnTheKey("pdp_ExchangePopup")));
+    	Assert.assertEquals(true, pdp_ExchangePopup.isDisplayed());
+		Thread.sleep(2000);
+		
+		
+		WebElement pdp_ExchangePopupButton = driver.findElement(By.xpath(cr.valueOnTheKey("pdp_ExchangePopupButton")));
+    	Assert.assertEquals(true, pdp_ExchangePopupButton.isDisplayed());
+		Thread.sleep(2000);
+
+		prm.click(driver, cr.valueOnTheKey("pdp_ExchangePopupButton"), "xpath");
+		Thread.sleep(2000);
+		
+		WebElement pdp_ExchangeCarRegNum = driver.findElement(By.xpath(cr.valueOnTheKey("pdp_ExchangeCarRegNum")));
+    	Assert.assertEquals(true, pdp_ExchangeCarRegNum.isDisplayed());
+		Thread.sleep(2000);
+		
+	
+ }
+}
---
+++ src/test/resources/Feature/PDP_ExchangeBanner.feature
@@ -0,0 +1,11 @@
+Feature: Verify Exchange Banner on pdp
+
+Background:
+    Given user land on spinny website
+    When Wait for the application page to load completely
+    And user selects location
+    
+    Scenario: Validate the Exchange Banner on pdp
+    And user clicks on the browse car button
+    And user select car
+    And user scroll down and validate
