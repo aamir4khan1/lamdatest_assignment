ee04397389ae7dd41e46e41f07bda3a39c61d6ef
Aamir Shafiq <aamir.shafiq@spinny.com>
2024-10-10 13:12:41 +0530

    changeto change

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = {"src/test/resources/Feature"},
         glue = {"StepDefinition"},
         plugin = {"json:target/cucumber.json"},
+        
        // tags = {"@change"},
         monochrome = true)
 public class TestRunner {
