8eeb4ad5a441b79b587455eb90558b4350cec1b6
Aamir Shafiq <aamir.shafiq@spinny.com>
2024-10-10 13:17:00 +0530

    conflictresolved

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json"},
         // tags = {"@change"},
 
-       tags = {"@change"},
+       
 
         monochrome = true)
 public class TestRunner {
