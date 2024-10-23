aafc1fdbd5340b29fbe22af13208971ebc2ce828
ravina <ravina.arora@spinny.com>
2024-10-23 10:39:45 +0530

    body type, similar car, photo gallery fixes

--- src/test/java/StepDefinition/Echallan.java
+++ src/test/java/StepDefinition/Echallan.java
@@ -1,5 +1,6 @@
 package StepDefinition;
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.JavascriptExecutor;
 //import cucumberIntegrationTests.screens.iOS.IOSLoginScreen;
 import org.openqa.selenium.WebDriver;
