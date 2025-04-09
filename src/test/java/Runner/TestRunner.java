025391dee2ea372bc0b20957723729b528340a1b
Aamir Shafiq <aamir.shafiq@spinny.com>
2025-04-09 10:05:16 +0530

    [CW-13672] runnerupdate

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,7 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/Feature"},
         glue = {"StepDefinition"},
-        plugin = {"json:target/cucumber.json"},
+        		plugin = {
+        			    "pretty",
+        			    "html:target/cucumber-html-reports", // HTML report path
+        			    "json:target/cucumber-reports/CucumberTestReport.json" // JSON report
+        			},
         // tags = {"@change"},
 
        
