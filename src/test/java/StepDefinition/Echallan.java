b81ff14bad7b2a2422169f19b3c15090c46c6b84
Aamir Shafiq <aamir.shafiq@spinny.com>
2025-04-28 09:17:29 +0530

    [CW-13810] waitimprovedinechallan

--- src/test/java/StepDefinition/Echallan.java
+++ src/test/java/StepDefinition/Echallan.java
@@ -27,15 +27,15 @@ public class Echallan {
 	
 	@And("user slects check_e_challan")
 	public void user_slects_check_e_challan() throws InterruptedException{
-		Thread.sleep(2000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("hamburger"));
 		prm.click(driver, cr.valueOnTheKey("hamburger"), "xpath");
-		Thread.sleep(2000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("open_menu"));
 		WebElement open_menu = driver.findElement(By.xpath(cr.valueOnTheKey("open_menu")));
 		Assert.assertEquals(true, open_menu.isDisplayed());
-		Thread.sleep(2000);
+		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("document.getElementById('sideMenu').scroll(0,800)");
-		Thread.sleep(5000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("check_echallan"));
 		prm.clickByJavaScript(driver, cr.valueOnTheKey("check_echallan"), "xpath");
 		
 		
@@ -43,16 +43,16 @@ public class Echallan {
 	}
 	@And("user enters registration number")
 	public void user_enters_registration_number() throws InterruptedException {
-		Thread.sleep(2000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("registration_number"));
 		prm.sendTextToTextBox(driver, cr.valueOnTheKey("registration_number"),("xpath"),(cr.valueOnTheKey("Number_number")));
-		Thread.sleep(2000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("check_challan"));
 		prm.clickByJavaScript(driver, cr.valueOnTheKey("check_challan"), "xpath");
 		
 
 	}
 	@And("user lands on echallan page")
 	public void user_lands_on_echallan_page() throws InterruptedException {
-		Thread.sleep(5000);
+		wait.presenceOfElementLocated(driver, "xpath", cr.valueOnTheKey("echallan_product_page"));
 		WebElement echallan_product_page = driver.findElement(By.xpath(cr.valueOnTheKey("echallan_product_page")));
 		Assert.assertEquals(true, echallan_product_page.isDisplayed());
 	
